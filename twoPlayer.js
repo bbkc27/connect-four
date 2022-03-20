221763e88ef063396f139cf9e3f8ca3e98c852fa
bbkc27 <briana.j.casey@gmail.com>
2022-03-20 16:27:09 -0400

    Board Functionality Intact

--- twoPlayer.js
+++ twoPlayer.js
@@ -1,9 +1,18 @@
 
-//Global Variables
+//Reset Game Button Functionality for Easier Testing
+const resetGameButton = document.querySelector(".resetGame");
 
+const resetGameFunc = () => {
+    location.reload();
+}
+
+resetGameButton.addEventListener("click", resetGameFunc);
+
+
+//Global Variables
 let score = 0;
 let turns = 0;
-let currentPlayer= "Player One";
+let currentPlayer;
 
 
 
@@ -30,29 +39,40 @@ howToPlayButton.addEventListener("click", openHowToPlay);
 closeHowToPlayButton.addEventListener("click", closeHowToPlay);
 
 
+
 /* --------- CURRENT PLAYER FUNCTIONALITY --------- */
 
-// Grabbing Current Player element
-let currentPlayerText = document.getElementById("currentPlayer");
+// Grabbing Game Elements
+let currentPlayerText = document.querySelector("#currentPlayer");
 let currentPlayerToken = document.querySelector(".currentPlayerToken");
-
-currentPlayerToken.style.backgroundColor = "red";
-
-
-if (turns % 2 == 0){
-    currentPlayer = "Player One"; 
-} else if (turns % 2 !==0){
-    currentPlayer = "Player Two";    
-}
-
-currentPlayerText.innerHTML = currentPlayer;
-
-
-
-
-//New Turn Functionality
 let boxes = document.querySelectorAll(".box");
 
+//Grabbing Game Board Columns
+const columnOne = document.querySelector(".columnOne");
+const columnTwo = document.querySelector(".columnTwo");
+const columnThree = document.querySelector(".columnThree");
+const columnFour = document.querySelector(".columnFour");
+const columnFive = document.querySelector(".columnFive");
+const columnSix = document.querySelector(".columnSix");
+const columnSeven = document.querySelector(".columnSeven");
+
+currentPlayerText.innerHTML = 'Player One';
+currentPlayerToken.style.backgroundColor = 'red';
+
+//FUNCTION FOR CHANGING CURRENT PLAYER DEFINED
+const setCurrentPlayer = () => {
+    if (turns % 2 == 0){
+           currentPlayer = "Player One"; 
+           currentPlayerText.innerHTML = "Player One";
+           currentPlayerToken.style.backgroundColor = 'red';
+       } else if (turns % 2 == 1){
+           currentPlayer = "Player Two"; 
+           currentPlayerText.innerHTML = "Player Two"  
+           currentPlayerToken.style.backgroundColor = 'green';
+       }
+}
+
+/* --------- DROP FUNCTIONALITY ---------  */
 //Functions making Boxes Droppable
 const allowDrop = (ev) => {
     ev.preventDefault();
@@ -61,35 +81,65 @@ const allowDrop = (ev) => {
 //Dropping new token
 const drop = (ev) => {
 
-    ev.preventDefault();
+ev.preventDefault();  
 
-    turns += 1
+    //IF statement so tokens can't stack on top of each other or have more than one token dropped in a box
+    if (ev.target.classList[0] === 'droppedToken' || ev.target.children.length > 0){
 
-    let token = document.createElement('div')
+        console.log("Can't drop ball here.")
+
+    //ELSE statement to create token if an open column is selected    
+    }else if (ev.target.children.length < 1){
+        ev.preventDefault();
+
+             let token = document.createElement('div')
     
-        if (currentPlayer = 'Player One'){
-            token.style.backgroundColor = "red";
-            token.classList.add("droppedToken")
-        } else if (currentPlayer = 'Player Two'){
-            token.style.backgroundColor = 'green';
-            token.classList.add("droppedToken")
-        }
+                 if (turns % 2 == 0){
+                     token.style.backgroundColor = "red";
+                     token.classList.add("droppedToken")
+                 } else if (turns % 2 == 1){
+                     token.style.backgroundColor = 'green';
+                     token.classList.add("droppedToken")
+                }
 
-    console.log(token)
+    //IF ELSE STATEMENTS SO BALL DROPS TO LOWEST EMPTY SLOT
+            if (ev.target.parentElement.children[0].children.length < 1){
 
-    //Setting Current Player
-    if (turns == 0 || turns % 2 == 0){
-    currentPlayer = "Player One"
-    currentPlayerText.innerText = currentPlayer;
-    currentPlayerToken.style.backgroundColor = "red";
-    } else if (turns % 2 !==0){
-    currentPlayer = "Player Two"
-    currentPlayerText == currentPlayer;
-    currentPlayerToken.style.backgroundColor = "green";
+                ev.target.parentElement.children[0].appendChild(token);
+                turns += 1
+                setCurrentPlayer(); 
+            } else if (ev.target.parentElement.children[1].children.length < 1){
+                
+                ev.target.parentElement.children[1].appendChild(token);
+                turns += 1
+                setCurrentPlayer(); 
+            } else if (ev.target.parentElement.children[2].children.length < 1){
+
+                ev.target.parentElement.children[2].appendChild(token);
+                turns += 1
+                setCurrentPlayer(); 
+            } else if (ev.target.parentElement.children[3].children.length < 1){
+                
+                ev.target.parentElement.children[3].appendChild(token);
+                turns += 1
+                setCurrentPlayer(); 
+            } else if (ev.target.parentElement.children[4].children.length < 1){
+
+                ev.target.parentElement.children[4].appendChild(token);
+                turns += 1
+                setCurrentPlayer(); 
+            } else if (ev.target.parentElement.children[5].children.length < 1){
+                
+                ev.target.parentElement.children[5].appendChild(token);
+                turns += 1
+                setCurrentPlayer(); 
+            }
     }
 
-    console.log(turns, currentPlayer, currentPlayerText.innerHTML)
-    return turns;
+    //CHECK FOR WIN CONDITION USING # OF TURNS
+
+    
+    console.log(turns)
 }
 
-
+console.log(gameBoard)
